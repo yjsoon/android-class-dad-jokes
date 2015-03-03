aba94d3b611aad8e4b194ee030484109105bc75c
YJ Soon <yjsoon@tinkertanker.com>
2015-03-03 14:09:32 +0800

    Added sample data and ArrayAdapter

--- app/src/main/java/com/tinkercademy/yjsoon/dadjokes/JokesListActivity.java
+++ app/src/main/java/com/tinkercademy/yjsoon/dadjokes/JokesListActivity.java
@@ -2,14 +2,26 @@ package com.tinkercademy.yjsoon.dadjokes;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 
 public class JokesListActivity extends ListActivity {
+
+    private String[] mSampleJokes= {"The one about a donkey",
+                                    "This is a great one",
+                                    "You won't believe your eyes when you read this",
+                                    "This cured my flu"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes_list);
+
+        ArrayAdapter<String> adapter;
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mSampleJokes);
+        setListAdapter(adapter);
+
     }
 
 }
