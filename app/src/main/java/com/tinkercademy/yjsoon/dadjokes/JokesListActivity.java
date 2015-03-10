f6ae7dc2e7aafcc61cf6e820d369fb8a52427c90
YJ Soon <yjsoon@tinkertanker.com>
2015-03-10 11:13:04 +0800

    Added list of jokes, but displays only memory addresses

--- app/src/main/java/com/tinkercademy/yjsoon/dadjokes/JokesListActivity.java
+++ app/src/main/java/com/tinkercademy/yjsoon/dadjokes/JokesListActivity.java
@@ -7,19 +7,20 @@ import android.widget.ArrayAdapter;
 
 public class JokesListActivity extends ListActivity {
 
-    private String[] mSampleJokes= {"The one about a donkey",
-                                    "This is a great one",
-                                    "You won't believe your eyes when you read this",
-                                    "This cured my flu"};
+    private Joke[] mJokes = {
+        new Joke("Can crusher", "Why did the can crusher quit his job?", "Because it was so depressing"),
+        new Joke("Nose", "What do you call someone with a nose but no body?", "Nobody knows"),
+        new Joke("Muffler", "Last night I dreamt I was a muffler", " I woke up exhausted")
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes_list);
 
-        ArrayAdapter<String> adapter;
+        ArrayAdapter<Joke> adapter;
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mSampleJokes);
+        adapter = new ArrayAdapter<Joke>(this, android.R.layout.simple_list_item_1, mJokes);
         setListAdapter(adapter);
 
     }
