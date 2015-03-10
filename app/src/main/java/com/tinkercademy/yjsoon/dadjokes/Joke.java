4adc3607481e0834f628464889850e3712b2569e
YJ Soon <yjsoon@tinkertanker.com>
2015-03-10 10:43:11 +0800

    Added Joke.java

---
+++ app/src/main/java/com/tinkercademy/yjsoon/dadjokes/Joke.java
@@ -0,0 +1,51 @@
+package com.tinkercademy.yjsoon.dadjokes;
+
+/**
+ * Created by yingjie on 10/3/15.
+ */
+public class Joke {
+
+    protected String mTitle;
+    protected String mQuestion;
+    protected String mAnswer;
+    protected boolean mSeen;
+
+    public Joke(String title, String question, String answer) {
+        mTitle = title;
+        mQuestion = question;
+        mAnswer = answer;
+        mSeen = false;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(String question) {
+        mQuestion = question;
+    }
+
+    public String getAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(String answer) {
+        mAnswer = answer;
+    }
+
+    public boolean isSeen() {
+        return mSeen;
+    }
+
+    public void setSeen(boolean seen) {
+        mSeen = seen;
+    }
+}
